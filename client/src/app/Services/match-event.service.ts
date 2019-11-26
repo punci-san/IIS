ae0a58b7383e6d20035d2062866d3b95140e0be4
Punči-san <skuta.matus@gmail.com>
2019-11-26 05:24:56 +0100

    Spider and match events update

--- client/src/app/Services/match-event.service.ts
+++ client/src/app/Services/match-event.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { UserService } from './user.service';
 import { matchEventPage } from '../../../../settings/routes';
+import { IMatchEvent } from '../../../../interfaces/match_event';
 
 @Injectable({
   providedIn: 'root'
@@ -41,6 +42,53 @@ export class MatchEventService {
       };
 
       http.send(JSON.stringify(data));
+    });
+  }
+
+  public getMatchEvents(matchID: number): Promise<IMatchEvent[]> {
+    return new Promise((resolve, reject) => {
+      const http = new XMLHttpRequest();
+      const page = `${matchEventPage}/${matchID}`;
+
+      http.open('GET', page, true);
+
+      this.userService.addAuthentication(http);
+
+      http.onreadystatechange = () => {
+        if (http.readyState === 4 && http.status === 200) {
+          try {
+            const matchEvents: IMatchEvent[] = JSON.parse(http.responseText);
+            return resolve(matchEvents);
+          } catch (error) {
+            return reject();
+          }
+        } else if (http.readyState === 4 && http.status !== 200) {
+          return reject();
+        }
+      };
+
+      http.send(null);
+    });
+  }
+
+  public deleteMatchEvent(matchEventID: number): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const http = new XMLHttpRequest();
+      const page = `${matchEventPage}/${matchEventID}`;
+
+      http.open('DELETE', page, true);
+
+      this.userService.addAuthentication(http);
+
+      http.onreadystatechange = () => {
+        if (http.readyState === 4 && http.status === 200) {
+          return resolve();
+        } else if (http.readyState === 4 && http.status !== 200) {
+          return reject();
+        }
+      };
+
+      http.send(null);
     });
   }
 }
